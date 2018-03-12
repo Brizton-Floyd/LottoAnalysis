4ae50f419b24fa61fa2a7b3ae16fccc018407b18
Brizton-Floyd <brizton.floyd@yahoo.com>
2018-03-12 15:40:46 -0700

    Create BetSlipDistributionAnalyzer.java
    new file

---
+++ src/com/lottoanalysis/utilities/betsliputilities/BetSlipDistributionAnalyzer.java
@@ -0,0 +1,93 @@
+package com.lottoanalysis.utilities.betsliputilities;
+
+import java.util.*;
+
+/**
+ * 
+ *
+ */
+@SuppressWarnings("unchecked")
+public class BetSlipDistributionAnalyzer{
+
+    private Integer formatHits; 
+    private Integer formatGamesOut;
+    private Map<String,Integer[]> winningNumberDistributionHolder;
+
+    public BetSlipDistributionAnalyzer(int formatHits, int formatGamesOut, Map<String, Integer[]> winningNumberDistributionHolder){
+
+        this.formatHits = formatHits; 
+        this.formatGamesOut = formatGamesOut; 
+        this.winningNumberDistributionHolder = winningNumberDistributionHolder; 
+    }
+
+    // setters
+    public void setFormatHits( int val ){
+        this.formatHits = val;
+    }
+
+    public void setFormatGamesOut( int val ){
+        this.formatGamesOut = val;
+    }
+
+    public void setWinningNumberDistributionHolder( Map<String, Integer[]> winningNumberDistributionHolder){
+        this.winningNumberDistributionHolder = winningNumberDistributionHolder;
+    }
+
+    // Getters 
+    public Integer getFormatHits(){
+        return this.formatHits; 
+    }
+
+    public Integer getFormatGamesOut(){
+        return this.formatGamesOut; 
+    }
+
+    public Map<String,Integer[]> getWinningNumberDistributionHolder(){
+        return this.winningNumberDistributionHolder;
+    }
+
+    /*
+    *
+    * 
+    */
+    public void incrementGamesOutForAllOtherFormatOccurences( Map<String, BetSlipDistributionAnalyzer> formats, String currentFormat){
+
+        formats.forEach( (key,value) -> {
+
+            if(!key.equals(currentFormat)){
+
+                BetSlipDistributionAnalyzer betSlipDistributionAnalyzer = value.getValue();
+                int gamesOut = betSlipDistributionAnalyzer.getFormatGamesOut().intValue();
+                betSlipDistributionAnalyzer.setFormatGamesOut( ++gamesOut );
+            }
+        });
+    }
+    /*
+    *
+    * 
+    */
+    public void incrementGamesOutForNonWinningAndIncrementWinningDistribution( Map<String, BetSlipDistributionAnalyzer> formats, String winningDistro){
+
+        formats.forEach( (k,v) -> {
+
+            BetSlipDistributionAnalyzer betSlipDistributionAnalyzer = v.getValue();
+            Map<String,Integer[]> data = betSlipDistributionAnalyzer.getWinningNumberDistributionHolder();
+
+            for(Map.Entry<String, Integer[]> values : data.entrySet()){
+
+                if(!values.getKey().equals(winningDistro)){
+
+                    Integer[] vals = values.getValue(); 
+                    vals[1]++;
+                }
+                else{
+
+                    Integer[] vals = values.getValue(); 
+                    vals[0]++;
+                    vals[1] = 0;                   
+                }
+            }
+        });
+
+    }
+}
