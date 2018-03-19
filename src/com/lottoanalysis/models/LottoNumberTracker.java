676013f40ec81d48160ae6374a5d67061f26bc02
Brizton-Floyd <brizton.floyd@yahoo.com>
2018-03-19 13:39:48 -0700

    Update LottoNumberTracker.java

--- src/com/lottoanalysis/models/LottoNumberTracker.java
+++ src/com/lottoanalysis/models/LottoNumberTracker.java
@@ -2,8 +2,7 @@ package com.lottoanalysis.models;
 
 import com.lottoanalysis.utilities.analyzerutilites.NumberAnalyzer;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class LottoNumberTracker {
 
@@ -21,6 +20,28 @@ public class LottoNumberTracker {
             data[0]++;
             data[1] = 0;
             NumberAnalyzer.incrementGamesOut(lottoNumberHitTracker,number);
+        }
+    }
+
+    public void insertHitsAndGamesOutForLottoNumbers(List<Map<Integer,Integer[]>> data ){
+
+        for(Map<Integer,Integer[]> numData : data){
+
+            for(Map.Entry<Integer,Integer[]> entry : numData.entrySet()){
+
+                if(lottoNumberHitTracker.containsKey( entry.getKey() )){
+
+                    Integer[] hitInfo = lottoNumberHitTracker.get( entry.getKey() );
+                    Integer[] incomingInfo = entry.getValue();
+                    incomingInfo[0] = hitInfo[0];
+                    incomingInfo[1] = hitInfo[1];
+                }
+                else{
+                    Integer[] incomingInfo = entry.getValue();
+                    incomingInfo[0] = -1;
+                    incomingInfo[1] = -1;
+                }
+            }
         }
     }
 }
